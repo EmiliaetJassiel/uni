8c31b08a1ba932cc642aee0f39d96f9635765699
EmiliaetJassiel <102743537+EmiliaetJassiel@users.noreply.github.com>
2022-10-18 13:44:56 -0400

    #

---
+++ Fundamentos de Programación (Si - 120)/Grupo A/Examen 1 (9-9-2022)/2/main.cpp
@@ -0,0 +1,20 @@
+/*
+2. Un supermercado va a implementar una política de descuentos a sus clientes de acuerdo con el monto de sus compras, de la siguiente forma:
+
+Compras:            Descuento: 
+0 a 500bs           0%
+501 a 1000bs        5%
+1001 a 2000bs       10%
+2001 en adelante    20%
+
+Al finalizar imprime el descuento y el total a pagar.
+*/
+
+#include <iostream>
+
+using namespace std;
+int main() {
+    
+
+    return 0;
+}
